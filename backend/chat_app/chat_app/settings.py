78deb7d01e67dc9ef9b2fc6839b862ccb03559ba
tO0masz <tomasz.goczol@wp.pl>
2025-08-14 16:42:27 +0200

    change: change db to postgres

--- backend/chat_app/chat_app/settings.py
+++ backend/chat_app/chat_app/settings.py
@@ -87,8 +87,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'somepassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
